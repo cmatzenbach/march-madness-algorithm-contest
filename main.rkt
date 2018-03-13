d33ab22397bf768baaa4183d9877b46d14b3c7f0
cmatzenbach <cmatzenbach@gmail.com>
2018-03-12 22:02:11 -0500

    Refactored code and added section headings

--- main.rkt
+++ main.rkt
@@ -1,12 +1,19 @@
 #lang racket
 (require csv-reading)
 
-(define (compare-teams team1 team2)
-  (cond ((compute-score team1) > (compute-score team2)) ((print "Hallo"))))
+;; Globals
+(define off-eff (csv->list (open-input-file "data/offensive-efficiency.csv")))
+(define def-eff(csv->list (open-input-file "data/defensive-efficiency.csv")))
+(define adj-eff (csv->list (open-input-file "data/adjusted-efficiency-margin.csv")))
 
-(define (compute-score team)
-  (print "world"))
-(print "Hello")
+;; Data Interaction
+(define (return-matching-row lst data)
+  (ormap (lambda (row)
+           (and (member data row) row)) lst))
+
+(define (get-rank stat team)
+  (let ([data (return-matching-row stat team)])
+    (car data)))
 
 (define make-bbdata-csv-reader
   (make-csv-reader-maker
@@ -17,21 +24,21 @@
 (define next-row
   (make-bbdata-csv-reader (open-input-file "data/offensive-efficiency.csv")))
 
-(next-row)
-(next-row)
-(next-row)
-(next-row)
-(next-row)
-(define off-eff (csv->list (open-input-file "data/offensive-efficiency.csv")))
-(define def-eff(csv->list (open-input-file "data/defensive-efficiency.csv")))
+;; Scoring
+(define (compare-teams team1 team2)
+  (cond ((compute-score team1) > (compute-score team2)) ((print "Hallo"))))
 
-(define (return-matching-row lst data)
-  (ormap (lambda (row)
-           (and (member data row) row)) lst))
+(define (compute-score team)
+  (print "world"))
+(print "Hello")
 
-(define (get-rank stat team)
-  (let ([data (return-matching-row stat team)])
-    (car data)))
+
+;; Testing
+(next-row)
+(next-row)
+(next-row)
+(next-row)
+(next-row)
 
 (return-matching-row off-eff "Ohio State ")
 (get-rank off-eff "Ohio State ")
